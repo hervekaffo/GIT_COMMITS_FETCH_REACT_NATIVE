abbc9badc699f7eddaf61478f850aa8691e509c6
Herve Kaffo <kaffo@Herves-MacBook-Pro.local>
2021-09-26 22:27:16 -0500

    Bringing the data in the Commits screen using a get request with axios

--- Screens/Commits.js
+++ Screens/Commits.js
@@ -1,12 +1,57 @@
-import React from 'react'
-import { View, Text} from 'react-native'
+import React, { useState} from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+} from "react-native";
 
-const Commits = () => {
-    return (
-        <View>
-            <Text>Commits Screen</Text>
-        </View>
-    )
-}
+import axios from "axios";
 
-export default Commits
+
+const Commits = (props) => {
+  const [commits, setCommits] = useState([]);
+
+  //api call
+  axios
+    .get(`https://api.github.com/repos/mojombo/grit/commits`)
+    .then((res) => {
+      setCommits(res.data);
+    })
+    .catch((error) => {
+      console.log('Api call error')
+    })
+
+
+
+  return (
+    <ScrollView>
+          <View style={styles.container}>
+
+            {commits? 
+                commits.map((commit)=>(
+                    <View style={{marginBottom:30}}>
+                        <Text>{commit.author.avatar_url}</Text> 
+                        <Text>{commit.commit.author.name}</Text>
+                        <Text>{commit.commit.message}</Text> 
+                    </View>
+                ))
+            :<Text>No Commits found</Text> }
+            {/* <Text>{commit.author.avatar_url}</Text>
+            <Text>{commit.commit.author.name}</Text>
+            <Text>{commit.sha}</Text>
+            <Text>{commit.commit.message}</Text> */}
+          </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+  container: {
+    marginBottom: 160,
+    backgroundColor: "white",
+  }
+});
+
+export default Commits;
